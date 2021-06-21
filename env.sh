b5591915e98f68483495f67dc1380d43f341c10c
garzoand <garzoand@gmail.com>
2021-06-21 07:05:03 -0700

    cm

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# You should run this as follows: ". ./env.sh"
+export GOOGLE_APPLICATION_CREDENTIALS="c:\Users\xxx\Download\xxx.json"
+export GOOGLE_PROJECT="tf-gcp-student-XXX"
+# on Windows, use the set / setx command instead of export
