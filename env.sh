9eee1742b3354bf2b26165a09934813778bfddf6
András Garzó <garzoand@gmail.com>
2021-06-21 07:15:00 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,4 +2,3 @@
 # You should run this as follows: ". ./env.sh"
 export GOOGLE_APPLICATION_CREDENTIALS="c:\Users\xxx\Download\xxx.json"
 export GOOGLE_PROJECT="tf-gcp-student-XXX"
-# on Windows, use the set / setx command instead of export
