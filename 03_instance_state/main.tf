200c004bd20d6dbff90299cb671d7293ac9e077f
garzoand <garzoand@gmail.com>
2021-06-22 06:18:13 -0700

    cm

--- 03_instance_state/main.tf
+++ 03_instance_state/main.tf
@@ -2,13 +2,20 @@ provider "google" {
     zone   = "us-central1-c"
 }
 
+variable "instance_name" {
+    description = "The name of the compute engine instance to be provisioned"
+    type        = string
+    # default     = "terraform-instance"
+}
+
 # resource <resource_type> <internal_resource_id> { ... properties ... }
 # Provision a GCP Compute Engine instance with a public ip address
 # to have access from outside
 # Link to doc of the resource: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
 resource "google_compute_instance" "instance" {
 
-    name = "terraform-instance"
+    # name = "terraform-instance"
+    name         = var.instance_name
     machine_type = "f1-micro"
 
     boot_disk {
