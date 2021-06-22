7b9173af92106706a1a084e2ea1eeb599a13bf74
garzoand <garzoand@gmail.com>
2021-06-22 06:29:24 -0700

    Cm

--- 03_input_output/main.tf
+++ 03_input_output/main.tf
@@ -2,12 +2,6 @@ provider "google" {
     zone   = "us-central1-c"
 }
 
-variable "instance_name" {
-    description = "The name of the compute engine instance to be provisioned"
-    type        = string
-    # default     = "terraform-instance"
-}
-
 # resource <resource_type> <internal_resource_id> { ... properties ... }
 # Provision a GCP Compute Engine instance with a public ip address
 # to have access from outside
@@ -39,4 +33,10 @@ output "instance_id" {
 output "instance_ip" {
     value = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
 }
-// ...
+
+variable "instance_name" {
+    description = "The name of the compute engine instance to be provisioned"
+    type        = string
+    # default     = "terraform-instance"
+}
+
