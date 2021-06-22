c653b8ac0d8dca2b29680e2ac4035f038d220277
garzoand <garzoand@gmail.com>
2021-06-22 06:11:20 -0700

    cm

--- 03_instance_state/main.tf
+++ 03_instance_state/main.tf
@@ -25,5 +25,11 @@ resource "google_compute_instance" "instance" {
     }
 }
 
+output "instance_id" {
+    value = google_compute_instance.instance.instance_id
+}
 
+output "instance_ip" {
+    value = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
+}
 // ...
