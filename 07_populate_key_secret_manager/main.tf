66823e963ef9ae9f2e932bbf1445a61b8440d620
garzoand <garzoand@gmail.com>
2021-06-22 08:17:00 -0700

    cm

--- 07_populate_key_secret_manager/main.tf
+++ 07_populate_key_secret_manager/main.tf
@@ -5,6 +5,10 @@ provider "google" {
 # Task: populate the public key (id_rsa.pub) as a new secret to Secret Manager
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/secret_manager_secret_version
 
+locals {
+    ssh_key = file("./id_rsa.pub")
+}
+
 resource "..." "..." {
 
 }
