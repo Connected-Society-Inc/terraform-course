e6a7b6723a278c88ce230ee6895b65075ebbd8de
garzoand <garzoand@gmail.com>
2021-06-23 07:47:17 -0700

    cm

---
+++ 09_multi_env/dev/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+    zone   = "us-central1-c"
+}
+
+# resource <resource_type> <internal_resource_id> { ... properties ... }
+resource "google_compute_instance" "instance" {
+
+    name = "instance-by-terraform-new"
+    machine_type = "f1-micro"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-10"
+        }
+    }
+
+    network_interface {
+        network = "default"
+    }
+}
+
+
+// ...
