f477035c2c762e36993942e6a610840040477adc
garzoand <garzoand@gmail.com>
2021-06-24 06:51:46 -0700

    cm

--- 13_condition/instance.tf
+++ 13_condition/instance.tf
@@ -3,6 +3,19 @@
 resource "google_compute_instance" "instance" {
 
     # <expression> ? <return on true> : <return on false>
+    #  ... is equal:
+    /*
+
+        var.create_instance ? length(var.subnets) : 0
+        ==== 
+        if var.create_instance == true
+        then
+          return length(var.subnets)
+        else 
+          return 0
+    */
+   
+    # this will return length(var.subnets) if create_instances is true, otherwise will be 0
     count = var.create_instances ? length(var.subnets) : 0
 
     name = var.instance_names[count.index]
