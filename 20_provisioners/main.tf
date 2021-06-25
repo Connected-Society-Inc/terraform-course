2c3ca699b14b9a308c4a0990cdd6b38b8cda4789
garzoand <garzoand@gmail.com>
2021-06-25 06:25:30 -0700

    cm

---
+++ 20_provisioners/main.tf
@@ -0,0 +1,48 @@
+resource "google_compute_instance" "instance" {
+
+    name         = "terraform-instance"
+    machine_type = "f1-micro"
+    zone         = "us-central1-c"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-10"
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config {} 
+    }
+
+    // We add a public key to be able to log in to the server via SSH
+    metadata = {
+        ssh-keys = "root:${file("~/.ssh/id_rsa.pub")}"
+    }
+
+    provisioner "remote-exec" {
+        inline = ["echo 'Hello from Provisioner!' >> /etc/motd"]
+        
+        connection {
+            type        = "ssh"
+            host        = self.network_interface[0].access_config[0].nat_ip
+            user        = "root"
+            private_key = file("~/.ssh/id_rsa")
+        }
+    }
+
+    // metadata_startup_script = "echo 'Hello from startup script!' >> /etc/motd"
+
+}
+
+resource "null_resource" "null_resource_example" {
+    provisioner "local-exec" {
+        command = "echo 'Hello world!'"
+        when    = destroy
+    }
+}
+
+
+output "public_ip" {
+    value = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
+}
--- project_vpc_with_subnets/1_network.tf
+++ project_vpc_with_subnets/1_network.tf
@@ -20,7 +20,7 @@ resource "google_compute_subnetwork" "subnets" {
     for_each = var.subnets
 
     name    = each.key
-    network = google_compute_network.vpc.self_link
+    network = google_compute_network.vpc.self_link // == .id
     region  = var.region
     
     ip_cidr_range = each.value.cidr_block
